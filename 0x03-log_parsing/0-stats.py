435232449a16b3a966e5bcc36f2ad7b2df5fef43
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-01-19 12:34:16 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-
+"""
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
+"""
 import sys
 from collections import defaultdict
 
