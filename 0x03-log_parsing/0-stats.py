80e5a0b7de64042a75976b40cb2c8edca4b9e013
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-01-19 12:33:19 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 
 import sys
+from collections import defaultdict
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+status_codes = defaultdict(int)
+file_size = 0
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split(" ")
-        if len(parts) != 9:
-            continue
-        try:
-            status_code = int(parts[8])
-            file_size = int(parts[9])
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            total_size += file_size
-        except ValueError:
-            continue
-        if line_count % 10 == 0:
-            print("Total file size:", total_size)
-            for code in sorted(status_codes.keys()):
-                if status_codes[code] > 0:
-                    print(f"{code}: {status_codes[code]}")
-except KeyboardInterrupt:
-    print("Total file size:", total_size)
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+def print_stats():
+    print("File size:", file_size)
+    for code, count in status_codes.items():
+        if count > 0:
+            print(code + ":", count)
 
+for line in sys.stdin:
+    parts = line.split(" ")
+    try:
+        status_code = parts[-2]
+        file_size += int(parts[-1])
+        status_codes[status_code] += 1
+    except:
+        pass
+    if sum(status_codes.values()) % 10 == 0:
+        print_stats()
+
+print_stats()
