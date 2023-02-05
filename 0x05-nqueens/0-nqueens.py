5e47c76571655d0a47421d2fc6a8d55a970b89fa
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-05 17:28:36 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,51 +1,44 @@
 #!/usr/bin/python3
-"""
-N queens puzzle
-"""
+""" N queens """
 import sys
 
 
-def solve_n_queens(n):
-    """ Backtracking algorithm to solve the N-Queens problem """
-    def can_place(row, col):
-        """
-        checks if a queen can be placed in a particular row
-        """
-        for i in range(row):
-            if board[i] == col or abs(board[i] - col) == abs(i - row):
-                return False
-        return True
-
-    def backtrack(row = 0, placed = []):
-        """
-        recursive function that places queens in each row
-        """
-        if row == n:
-            result.append(placed[:])
-        for col in range(n):
-            if can_place(row, col):
-                board[row] = col
-                backtrack(row + 1, placed + [col])
-
-    board = [-1] * n
-    result = []
-    backtrack()
-    return result
-
-if len(sys.argv) != 2:
+if len(sys.argv) > 2 or len(sys.argv) < 2:
     print("Usage: nqueens N")
-    sys.exit(1)
+    exit(1)
 
-try:
-    n = int(sys.argv[1])
-except ValueError:
+if not sys.argv[1].isdigit():
     print("N must be a number")
-    sys.exit(1)
+    exit(1)
 
-if n < 4:
+if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    sys.exit(1)
+    exit(1)
 
-result = solve_n_queens(n)
-for solution in result:
-    print(" ".join(str(x) for x in solution))
+n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
