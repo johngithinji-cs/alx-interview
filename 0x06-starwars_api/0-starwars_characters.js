5c269577aeb1fdcebc1dd49faa69860e0b3ebd75
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-10 14:57:40 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,39 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
 
-const apiUrl = `https://swapi.co/api/films/${movieId}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`An error occurred: ${error}`);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to retrieve movie data. Response code: ${response.statusCode}`);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-
-  characters.forEach((character) => {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.error(`An error occurred: ${error}`);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error(`Failed to retrieve character data. Response code: ${response.statusCode}`);
-        return;
-      }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
-  });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
-
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
