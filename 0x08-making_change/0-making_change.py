c1315ac9eac234b78101708248008835e3336745
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2023-02-22 17:31:33 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,16 +3,16 @@
 
 
 def makeChange(coins, total):
-     """Clasic Bottom-Up dynamic programming"""
-    coins = sorted(coins, reverse=True)
-    count = 0
-    
+    """Clasic Bottom-Up dynamic programming"""
+    temp_value = 0
+    coins.sort(reverse=True)
+
+    if total < 0:
+        return 0
+
     for coin in coins:
-        while total >= coin:
-            total -= coin
-            count += 1
-            
-    if total == 0:
-        return count
-    else:
-        return -1 if count == 0 else count
+        if total % coin <= total:
+            temp_value += total // coin
+            total = total % coin
+
+    return temp_value if total == 0 else -1
